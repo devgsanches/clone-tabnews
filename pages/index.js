9b0520342dd48d601b0a231eda55b66dd437218a
Guilherme Sanches <160994960+devgsanches@users.noreply.github.com>
2024-08-12 14:30:11 +0000

    subindo arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Realmente amor, ninguém merece 😔</h1>;
 }
 
-function teste() {
-  console.log("indentação errada");
-}
-
-function teste2() {
-  console.log("eaa");
-}
-
 export default Home;
